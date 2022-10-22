392f01c18006217639a2f80511e4bf992ed67ecf
한상희 <70365377+SangHui48@users.noreply.github.com>
2022-10-22 14:45:38 +0900

    10/22
    알고리즘 문제 풀이중

--- 이것이 코딩테스트다/greedy/q3 문자열 뒤집기.py
+++ 이것이 코딩테스트다/greedy/q3 문자열 뒤집기.py
@@ -1,14 +1,41 @@
+# s = input()
+#
+# cnt_zero = 0
+# cnt_one = 0
+#
+# for i in s:
+#     if i == '0':
+#         cnt_zero += 1
+#     elif i == '1':
+#         cnt_one += 1
+#
+#
+#
+# print(min(cnt_one, cnt_zero))
+
+# s = input()
+# cnt0 = 0
+# cnt1 = 0
+#
+# if s[0] == '1':
+#     cnt0 += 1
+# else:
+#     cnt1 += 1
+#
+# for i in range(1, len(s)-1):
+#     if s[i] != s[i+1]:
+#         if s[i + 1] == '1':
+#             cnt0 += 1
+#         else:
+#             cnt1 += 1
+#
+# print(min(cnt0, cnt1))
+
 s = input()
+cnt = 0
 
-cnt_zero = 0
-cnt_one = 0
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        cnt +=1
 
-for i in s:
-    if i == '0':
-        cnt_zero += 1
-    elif i == '1':
-        cnt_one += 1
-
-
-
-print(min(cnt_one, cnt_zero))
+print((cnt + 1) // 2)
---
+++ 이것이 코딩테스트다/greedy/q5 볼링공 고르기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+result = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        if data[i] != data[j]:
+            result += 1
+
+print(result)
