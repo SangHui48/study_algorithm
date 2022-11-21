bc45a772cabec0d28bd96b4c5b725da45edecd09
SangHui48 <vodlrm123@naver.com>
2022-11-21 22:34:37 +0900

    11/21
    do it식 삽입정렬

--- do it! 알고리즘 코딩 테스트/04_정렬/018_ATM 인출 시간 계산하기.py
+++ do it! 알고리즘 코딩 테스트/04_정렬/018_ATM 인출 시간 계산하기.py
@@ -12,6 +12,19 @@ s = [0] * n
 #             min_index = j
 #     data[i] , data[min_index] = data[min_index], data[i]
 
+for i in range(1, n):
+    insert_point = i
+    insert_value = data[i]
+    for j in range(i-1, -1, -1):
+        if data[j] < data[i]:
+            insert_point = j + 1
+            break
+        if j == 0:
+            insert_point = 0
+    for j in range(i, insert_point, -1):
+        data[i] = data[j-1]
+    data[insert_point] = insert_value
+
 s[0] = data[0]
 
 for i in range(1, n):
