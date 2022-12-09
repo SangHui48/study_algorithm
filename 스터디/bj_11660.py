0546b2606132a1a26b5c81e6a8cb69f32c9707ae
SangHui48 <vodlrm123@naver.com>
2022-12-10 01:31:27 +0900

    12/10
    수업

---
+++ 스터디/bj_11660.py
@@ -0,0 +1,7 @@
+# for start, end in range(): 3
+#     s = start, e = end
+#     sum = 0
+#     # for i in range(s, e): 5
+#     #     sum += data[i]
+#     # print(sum)
+    # 합배열을 만들었더니 질의 횟수 만큼으로 시간 복잡도가 줄었잖아.
