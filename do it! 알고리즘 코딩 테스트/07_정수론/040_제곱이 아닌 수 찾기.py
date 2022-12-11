6cd49b46a58f58c0d426c47e5f465a75e992eb8c
SangHui48 <vodlrm123@naver.com>
2022-12-11 16:10:53 +0900

    12/11
    알고리즘 문제 풀이

--- do it! 알고리즘 코딩 테스트/07_정수론/040_제곱이 아닌 수 찾기.py
+++ do it! 알고리즘 코딩 테스트/07_정수론/040_제곱이 아닌 수 찾기.py
@@ -1,2 +1,18 @@
-Min, Max = map(int, input().split())
+import math
 
+Min, Max = map(int, input().split())
+check = [False] * (Max - Min + 1)
+
+for i in range(2, int(math.sqrt(Max)) + 1):
+    start_val = i * i
+    start_index = int(Min/start_val)
+    if Min % start_val != 0:
+        start_index += 1
+    for j in range(start_index, int(Max/start_val) + 1):
+        check[int(j * start_val) - Min] = True
+
+answer = 0
+for i in range(0, Max - Min + 1):
+    if not check[i]:
+        answer += 1
+print(answer)
