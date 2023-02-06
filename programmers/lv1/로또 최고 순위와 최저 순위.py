5b17e09fbdc8de5fadd8ccfb71934b9f913d1abe
SangHui48 <vodlrm123@naver.com>
2023-02-06 19:36:04 +0900

    2/6
    효율적 풀이 추가

--- programmers/lv1/로또 최고 순위와 최저 순위.py
+++ programmers/lv1/로또 최고 순위와 최저 순위.py
@@ -13,4 +13,15 @@ def solution(lottos, win_nums):
             possible += 1
     return [rank-possible, rank]
 
+
+# def solution(lottos, win_nums):
+#
+#     rank=[6,6,5,4,3,2,1]
+#
+#     cnt_0 = lottos.count(0)
+#     ans = 0
+#     for x in win_nums:
+#         if x in lottos:
+#             ans += 1
+#     return rank[cnt_0 + ans],rank[ans]
 print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
