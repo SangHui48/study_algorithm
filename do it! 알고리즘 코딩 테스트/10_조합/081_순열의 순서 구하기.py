6163d730debba1e239afb31bd198ebac7afc4044
SangHui48 <vodlrm123@naver.com>
2023-02-09 19:08:25 +0900

    2/9
    순열의 순서구하기

---
+++ do it! 알고리즘 코딩 테스트/10_조합/081_순열의 순서 구하기.py
@@ -0,0 +1,37 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n = int(input())
+dp = [0] * (n + 1)
+dp[0] = 1
+data = list(map(int, input().split()))
+visited = [False] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = dp[i-1] * i
+if data[0] == 1:
+    answer = [0] * (n + 1)
+    k = data[1]
+    for i in range(1, n+1):
+        cnt = 1
+        for j in range(1, n + 1):
+            if visited[j]:
+                continue
+            if k <= cnt * dp[n-i]:
+                x = dp[n-i]
+                k -= (cnt - 1) * dp[n-i]
+                answer[i] = j
+                visited[j] = True
+                break
+            cnt += 1
+    for i in range(1, n + 1):
+        print(answer[i], end=' ')
+elif data[0] == 2:
+    k = 1
+    for i in range(1, n + 1):
+        cnt = 0
+        for j in range(1, data[i]):
+            if not visited[j]:
+                cnt += 1
+        k += cnt * dp[n-i]
+        visited[data[i]] = True
+    print(k)
