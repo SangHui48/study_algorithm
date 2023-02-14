91e276b9c33d56519a4a198cd380e22c32854e3d
SangHui48 <vodlrm123@naver.com>
2023-02-14 21:10:26 +0900

    2/14
    combination
    할수있다 아자아자아자아자

--- do it! 알고리즘 코딩 테스트/10_조합/082_사전찾기.py
+++ do it! 알고리즘 코딩 테스트/10_조합/082_사전찾기.py
@@ -1,2 +1,26 @@
 # https://www.acmicpc.net/problem/1256
-import sys
+# https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=junhyuk7272&logNo=222053814549
+# https://bhsmath.tistory.com/153
+n, m, k = map(int, input().split())
+dp = [[0 for _ in range(n + m + 1)] for _ in range(n + m + 1)]
+
+for i in range(0, n + m + 1):
+    for j in range(0, i + 1):
+        if j == 0 or j == i:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+            if dp[i][j] > 1000000000:
+                dp[i][j] = 1000000001
+
+if dp[n+m][m] < k:
+    print(-1)
+else:
+    while not (n == 0 and m == 0):
+        if dp[n + m - 1][m] >= k:
+            print('a', end='')
+            n -= 1
+        else:
+            print('z', end='')
+            k -= dp[n + m - 1][m]
+            m -= 1
