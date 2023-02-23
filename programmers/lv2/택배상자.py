e9bccc6e983cf20d5b41fc5b8125846367c801de
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-02-23 20:40:34 +0900

    2/23
    tmp

---
+++ programmers/lv2/택배상자.py
@@ -0,0 +1,12 @@
+from collections import deque
+def solution(order):
+    answer = 0
+    order = deque(order)
+    stack = []
+    for i in range(1, len(order) + 1):
+        stack.append(i)
+        while stack and order[0] == stack[-1]:
+            answer += 1
+            stack.pop()
+            order.popleft()
+    return answer
