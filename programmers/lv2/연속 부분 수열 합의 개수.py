a0fedc2344d2507b6b6f0a3bd36d71cc4dba1c89
SangHui48 <vodlrm123@naver.com>
2023-02-24 01:46:31 +0900

    2/24
    tmp

--- programmers/lv2/연속 부분 수열 합의 개수.py
+++ programmers/lv2/연속 부분 수열 합의 개수.py
@@ -1,1 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/131701
+def solution(elements):
+    n = len(elements)
+    elements = elements + elements
+    print(elements)
+    answer = set(elements)
+    answer.add(sum(elements))
+    for i in range(2, n):
+        for j in range(0, n+1):
+            answer.add(sum(elements[j:j+i]))
+    return len(answer)
+
+print(solution([7,9,1,1,4]))
