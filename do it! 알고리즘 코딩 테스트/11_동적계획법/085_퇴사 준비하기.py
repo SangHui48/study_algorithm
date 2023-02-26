d4eb379773aaa8a13802fb66c2c80062510e2638
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-02-26 20:20:43 +0900

    2/26
    백준 dp

--- do it! 알고리즘 코딩 테스트/11_동적계획법/085_퇴사 준비하기.py
+++ do it! 알고리즘 코딩 테스트/11_동적계획법/085_퇴사 준비하기.py
@@ -1,7 +1,20 @@
+# https://osnim.tistory.com/m/entry/%EB%B0%B1%EC%A4%80-14501-%ED%87%B4%EC%82%AC-Python
+
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 n = int(input())
-data = []
-for _ in range(n):
-    data.append(tuple(map(int, input().split())))
+dp = [0] * (n + 2)
+t = [0] * (n + 2)
+p = [0] * (n + 2)
+
+for i in range(1, n+1):
+    t[i], p[i] = map(int, input().split())
+
+for i in range(n, 0, -1):
+    if i + t[i] > n+1:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i + t[i]] + p[i])
+
+print(dp[1])
