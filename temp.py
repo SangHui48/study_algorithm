390a31b3ef1fbef64316b260253ce9abde297966
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-03-14 22:06:30 +0900

    3/14
    tmp

--- temp.py
+++ temp.py
@@ -1,2 +1,68 @@
 class Node:
-    def __
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self)
+        if self.right:
+            traversal += self.right.inorder()
+    
+    def min(self):
+        if self.left: # 제일 왼쪽 끝에 있는 노드가 제일 작은 노드
+            return self.left.min()
+        else:
+            return self # 그렇지 않다면 자기 자신을 리턴
+        
+    def max(self):
+        if self.right:
+            return self.right.max()
+        else:
+            return self
+    
+    def lookup(self, key, parent=None):
+        if key < self.key:
+            if self.left:
+                return self.left.lookup(key, self) # self = parent node
+            else:
+                return None, None
+        elif key > self.key:
+            if self.right:
+                return self.right.lookup(key, self)
+            else:
+                return None, None
+        else:
+            return self, parent
+
+class BinSearchTree:
+    def __init__(self):
+        self.root = None
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+        
+    def min(self):
+        if self.root:
+            return self.root.min()
+        else:
+            return None
+    
+    def max(self):
+        if self.root:
+            return self.root.max()
+        else:
+            return None
+    
+    def lookup(self, key):
+        if self.root:
+            return self.root.lookup(key)
+        else:
+            return None, None
