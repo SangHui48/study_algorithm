6bcf76453b695bc86785aa53c4f98265a1be603d
SangHui48 <vodlrm123@naver.com>
2023-03-16 02:42:05 +0900

    3/16
    프로그래머스ai

---
+++ programmers/lv2/더 맵게.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42626
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        min1 = heapq.heappop(scoville)
+        if min1 >= K:
+            break
+        elif len(scoville) == 0:
+            answer = -1
+            break
+        min2 = heapq.pop(scoville)
+        new_scoville = min1 + (min2 * 2)
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+    return answer
