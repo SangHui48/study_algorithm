4b3e0bd6886e9154bd74019f275e17ff0115887c
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-03-15 22:10:32 +0900

    3/15
    프로그래머스

--- programmers/lv2/큰 수 만들기.py
+++ programmers/lv2/큰 수 만들기.py
@@ -1,4 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42883
 def solution(number, k):
-    answer = ''
-    return answer
+    collected = [] # 여기에 숫자를 한자리씩 채움
+    for i, num in enumerate(number):
+        while len(collected) > 0 and collected[-1] < num and k > 0:
+            collected.pop()
+            k -= 1
+        if k == 0:
+            collected += list(number[i:])
+            break
+        collected.append(num)
+    # 만약 98765 인 경우면 위 로직에서는 뒤의 넘버가 현제의 넘버보다 클 경우에만 k를 줄이고 수를 빼주기 때문에 아무것도 남지 않을것 따라서 collected가
+    # number 리스트와 똑같이 생겻을 것 그 경우를 예외 처리 해주는 코드
+    collected = collected[:-k] if k > 0 else collected
+    return ''.join(collected)
