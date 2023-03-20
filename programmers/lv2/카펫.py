a257dd23313bd955658ac0ec086030953f4ce5fa
SangHui48 <vodlrm123@naver.com>
2023-03-20 21:41:57 +0900

    3/20

--- programmers/lv2/카펫.py
+++ programmers/lv2/카펫.py
@@ -1,4 +1,10 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42842
 def solution(brown, yellow):
     answer = []
+    for h in range(1, yellow + 1):
+        if yellow % h == 0:
+            w = yellow // h
+            if (w * 2) + 2 * (h + 2) == brown:
+                answer += [w + 2, h + 2]
+                break
     return answer
