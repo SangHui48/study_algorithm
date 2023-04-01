6a11e387bec571f5b7b0ea6efcafe4d9df644df9
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-04-01 11:58:27 +0900

    4/1

--- 이것이 코딩테스트다/implementation/q9_문자열 압축.py
+++ 이것이 코딩테스트다/implementation/q9_문자열 압축.py
@@ -64,3 +64,24 @@ def solution(s):
     return answer
 
 print(solution("aabbaccc"))
+
+
+def solution(s):
+    answer = len(s)
+
+    for size in range(1, len(s) // 2 + 1):
+        count = 1
+        compress = 0
+        
+        prev = s[:size]
+        for i in range(size, len(s) + size, size):
+            curr = s[i:i + size]
+            if prev == curr:
+                count += 1
+            else:
+                compress += size + len(str(count)) if 1 < count else len(prev)
+                prev = curr
+                count = 1
+        answer = min(answer, compress)
+
+    return answer
