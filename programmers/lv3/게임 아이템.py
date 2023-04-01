a0a982d70936b5fe981545e4e3b89d9b49b38c76
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-04-01 12:18:57 +0900

    4/1

---
+++ programmers/lv3/게임 아이템.py
@@ -0,0 +1,23 @@
+import heapq
+from collections import deque
+
+def solution(healths, items):
+    healths.sort()
+    items = sorted([(item[1], item[0], index+1) for index, item in enumerate(items)])
+    items = deque(items)
+    answer = []
+    heap = []
+    for health in healths:
+        while items:
+            debuff, buff, index = items[0]
+            if health - debuff < 100:
+                break
+            items.popleft()
+            heapq.heappush(heap, (-buff, index))
+        if heap:
+            _, index = heapq.heappop(heap)
+            answer.append(index)
+    return sorted(answer)
+
+
+print(solution([200,120,150], [[30,100],[500,30],[100,400]]))
