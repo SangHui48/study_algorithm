1bb48aa0b3a3adfc33f4defeffdd1945a7b05cbe
SangHui48 <vodlrm123@naver.com>
2023-04-13 01:25:59 +0900

    4/13
    프로그래머스 lv2

--- programmers/lv2/멀리 뛰기.py
+++ programmers/lv2/멀리 뛰기.py
@@ -1,1 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12914
+def solution(n):
+    if n <= 3:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    return dp[n]
