892e48ef71e000db90b1155637c0efe7662d1e33
SangHui48 <vodlrm123@naver.com>
2023-04-19 06:29:16 +0900

    4/19
    프로그래머스 lv2

--- programmers/lv2/줄서는방법.py
+++ programmers/lv2/줄서는방법.py
@@ -1,1 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12936
+# https://hongcoding.tistory.com/55
+import math
+def solution(n, k):
+    answer = []
+    stack = [i for i in range(1, n+1)]
+    k = k - 1
+    while stack:
+        a = k // math.factorial(n-1)
+        answer.append(stack[a])
+        del stack[a]
+        k = k % math.factorial(n-1)
+        n -= 1
+    return answer
