12d1ed7b8a67355c25424ba43b75ff0c0227b1b5
SangHui48 <vodlrm123@naver.com>
2023-06-14 06:27:53 +0900

    6/14
    라면 사기

--- baekjoon/1815_라면사기.py
+++ baekjoon/1815_라면사기.py
@@ -5,4 +5,34 @@ input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
 A = list(map(int, input().split()))
+answer = 0
 
+for i in range(N-2):
+    if A[i+1] > A[i+2]:
+        m = min(A[i], A[i+1]-A[i+2])
+        answer += (m * 5)
+        A[i] -= m
+        A[i+1] -= m
+    
+    if A[i] > 0 and A[i+1] > 0 and A[i+2] > 0:
+        m = min(A[i], A[i+1])
+        answer += (m * 7)
+        A[i] -= m
+        A[i+1] -= m
+        A[i+2] -= m
+    
+    if A[i] > 0:
+        answer += (A[i] * 3)
+
+if A[-2] > 0 and A[-1] > 0:
+    m = min(A[-2], A[-1])
+    answer += (m * 5)
+    A[-2] -= m
+    A[-1] -= m
+
+if A[-2] > 0:
+    answer += (A[-2] * 3)
+else:
+    answer += (A[-1] * 3)
+
+print(answer)
