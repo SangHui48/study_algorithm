8064084998eab43f800fa9165bb5e9d208b0675b
SangHui48 <vodlrm123@naver.com>
2023-06-15 05:33:03 +0900

    6/15_solving

---
+++ baekjoon/17420_깊콘이넘쳐흘러.py
@@ -0,0 +1,10 @@
+# https://velog.io/@chang626/17420-%EA%B9%8A%EC%BD%98%EC%9D%B4-%EB%84%98%EC%B3%90%ED%9D%98%EB%9F%AC
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
